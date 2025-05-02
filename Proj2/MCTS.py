f1e947970e645e6465cf0cdaa33ea2b203a55ae6
quinthemint <quinn.potter93@gmail.com>
2025-05-01 18:01:15 -0700

    tree work

---
+++ Proj2/MCTS.py
@@ -0,0 +1,68 @@
+import math
+import random
+import time
+from handRank import ranker
+
+C = math.sqrt(2)
+
+class Node:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.visits = 0
+        self.wins = 0
+        self.terminal = False
+
+    def add_child(self, child_state):
+        child = Node(child_state, parent=self)
+        self.children.append(child)
+        return child
+    
+def UCB1(node):
+    (node.wins / node.visits) + C * math.sqrt(math.log(node.parent.visits) / node.vists)
+
+# tree search
+# start at our cards plus the current round's community cards
+def MCTS(root, deck, hand, table, round, limit = 10):
+    start = time.perf_counter()
+    while ((time.perf_counter() - start) <= limit):
+        node = root
+        deck_cpy = deck
+        # selection - find non terminal leaf with highest UCB1
+        while not node.terminal and node.children is not None:
+            UCB1s = [(child, UCB1(child)) for child in node.children]
+            node = max(UCB1s, key=lambda x: x[1])[0]
+
+        # expansion - expand from leaf with new states
+#       ∗ Simulate random possible opponent hole cards
+#       ∗ Simulate random future community cards
+
+        # pull opp cards
+        random.shuffle(deck_cpy)
+        opp = deck_cpy[-2:]
+        del (deck_cpy[-2:])
+
+        # pull remaining cards depending on round coming into tree
+        if round == 0:
+            table.append(deck_cpy[-5:])
+        elif round == 1:
+            table.append(deck_cpy[-2])
+        else:
+            table.append(deck_cpy[-1])
+
+        # simulation
+        # rank hand against opp
+
+        oppScore = ranker(opp + table)
+        botScore = ranker(hand + table)
+        win = 0
+        if botScore > oppScore:
+            win = 1
+
+        # backpropagation
+        while node is not None:
+            node.wins += win
+            node.visits += 1
+            node = node.parent
+
--- Proj2/poker.py
+++ Proj2/poker.py
@@ -1,7 +1,57 @@
+import random
+
 # cards are 0-51
 # suit is card // 13
 # rank is card % 13 
 
 # store deck as list, copy each simulation to make mutable and remove cards
 
-# TODO: deck management, monte carlo search tree
+# TODO: monte carlo search tree
+
+# each node holds independent wins and visits, so the sum at the root is win/simulation = win% for decision
+    
+def flop(deck):
+    flop = deck[-3:]
+    del deck[-3:]
+    return flop
+
+def turn(deck, flop):
+    flop.append(deck[44])
+    del deck[-1:]
+    return flop
+
+def river(deck, turn):
+    turn.append(deck[43])
+    del deck[-1:]
+    return turn
+
+deck = [x for x in range(52)]
+random.shuffle(deck)
+
+# deal (actual opp cards)
+bot = deck[-2:]
+opp = deck[-4:-2]
+del deck[-4:]
+
+print(bot)
+print(opp)
+
+# run mcts, decide if win is >50
+
+# actual flop
+flop = flop(deck)
+print(flop)
+
+# run mcts, decide if win is >50
+
+# actual turn
+turn = turn(deck, flop)
+print(turn)
+
+# run mcts, decide if win is >50
+
+# actual river
+river = river(deck, turn)
+print(river)
+
+# determine winner, or we folded
